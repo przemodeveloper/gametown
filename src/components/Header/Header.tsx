85920a892d56dd9c4a15a91e690c5aa9bf698ced
Przemysław Górecki <gorecki.p@hanaboso.com>
2022-02-14 15:58:49 +0100

    Create header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import classes from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <h1>gameTOWN</h1>
+    </header>
+  );
+};
+
+export default Header;
