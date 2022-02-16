9cb8fc8fba0f2cde43599e08f4b475cc160dae72
Przemysław Górecki <gorecki.p@hanaboso.com>
2022-02-16 10:28:55 +0100

    Add styles

--- src/components/Cart/PriceSummary.tsx
+++ src/components/Cart/PriceSummary.tsx
@@ -1,8 +1,9 @@
 import { useStore } from "../stores/game-context";
+import classes from "./PriceSummary.module.scss";
 
 const PriceSummary = () => {
   const { games } = useStore();
-  return <h1>Total amount: ${games.totalPrice}</h1>;
+  return <h1 className={classes.title}>Total amount: ${games.totalPrice}</h1>;
 };
 
 export default PriceSummary;
